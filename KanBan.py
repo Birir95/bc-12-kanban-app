d6b5e4691f8c5cdf3ba6d3e50b409bd0b5fe3c5c
Purity Birir <puritybirir@gmail.com>
2016-11-22 00:34:09 +0300

    Create a skeleton for the Kanban application including all the methods

---
+++ KanBan.py
@@ -0,0 +1,30 @@
+class ToDo(object):
+	"""KanBan is a console application that is used to manage to-do 
+	tasks using the KanBan way of organizing todo into 
+	3 sections: todo, doing, done.""" 
+	def __init__(self)
+	pass
+
+	def to_do(self,task_name,task_desc):
+		pass
+
+	def doing(self, taskid,task_start):
+		pass
+
+	def done(self,task_id,task_stop):
+		pass
+
+	def list_todo(self):
+		pass
+
+	def list_doing(self):
+		pass
+
+	def list_done(self):
+		pass
+
+	def list_all(self):
+		pass
+
+	def main():
+		pass
